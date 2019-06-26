6e33f69a87ce780307ec0027f40cdb4c52c2c070
Ibtesam <ibtesamansari070@gmail.com>
2019-06-26 12:10:29 +0530

    Title added

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Ibtesam | Portfolio',
       routes: <String, WidgetBuilder>{
         '/': (BuildContext contex) => MainPage(),
       },
